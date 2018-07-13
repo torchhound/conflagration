92ec679f5ea43a4bbb909495af4c03e6e448a16e
torchhound <unknownmasayoshi@gmail.com>
2018-07-12 23:26:28 -0400

    Removed generated files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import Catalog from './components/Catalog'
 import registerServiceWorker from './registerServiceWorker';
