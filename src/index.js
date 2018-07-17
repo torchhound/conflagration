1ab4d75421cd455c8583517694df0c31ac76f7f3
torchhound <unknownmasayoshi@gmail.com>
2018-07-17 05:14:16 -0400

    Got .env working

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ import imageboard from './actions/reducers';
 import thunk from 'redux-thunk';
 
 const config = {
-  apiKey: 'AIzaSyCVC54ShrF5teBwUl_le5p31eu3f4D-v84',
-  authDomain: 'conflagration-91331.firebaseapp.com',
-  databaseURL: 'https://conflagration-91331.firebaseio.com',
-  projectId: 'conflagration-91331',
-  storageBucket: 'conflagration-91331.appspot.com'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET
 };
 Firebase.initializeApp(config);
 
